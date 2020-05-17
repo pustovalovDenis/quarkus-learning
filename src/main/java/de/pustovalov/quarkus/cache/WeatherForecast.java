cf7ee7e7ab4c5bb5d87ca825ac844fc7dc17a5e6
Denis Pustovalov <Denis.Pustovalov@ist.com>
2020-05-17 14:06:53 +0300

    cacheing

---
+++ src/main/java/de/pustovalov/quarkus/cache/WeatherForecast.java
@@ -0,0 +1,24 @@
+package de.pustovalov.quarkus.cache;
+
+import java.util.List;
+
+public class WeatherForecast {
+
+    private List<String> dailyForecasts;
+
+    private long executionTimeInMs;
+
+    public WeatherForecast(List<String> dailyForecasts, long executionTimeInMs) {
+        this.dailyForecasts = dailyForecasts;
+        this.executionTimeInMs = executionTimeInMs;
+    }
+
+    public List<String> getDailyForecasts() {
+        return dailyForecasts;
+    }
+
+    public long getExecutionTimeInMs() {
+        return executionTimeInMs;
+    }
+
+}
